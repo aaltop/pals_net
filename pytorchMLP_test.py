7a7568b5ad3b47fa8ac3a63c122551cce389faf2
Topi Aaltonen <topi.aaltonen@helsinki.fi>
2023-03-29 18:01:52 +0300

    Change various little things
    Nothing specific, just more of a "save".

--- pytorchMLP_test.py
+++ pytorchMLP_test.py
@@ -89,6 +89,9 @@ def train_with_closure(model, inputs, outputs, batch_size, optim, loss_func):
     Used with optimisers that require passsing closure.
     '''
 
+    # TODO: this doesn't really work. The PyTorch documentation is
+    # rather vague on how this is supposed to be done.
+
     model.train()
 
     epoch_loss = 0
@@ -115,7 +118,7 @@ def train_with_closure(model, inputs, outputs, batch_size, optim, loss_func):
 
             loss = loss_func(pred, y)
             loss.backward()
-            # epoch_loss += loss.item()
+            epoch_loss += loss.item()
 
 
             return loss
@@ -132,10 +135,37 @@ def r2_score(pred,y):
             ((y-y.mean(dim=0))**2).sum()
         )
 
+
+def r2_evaluate(model, x_train, y_train, x_test, y_test, do_logging=False):
+    '''
+    Evaluates the R2 score, optionally does logging to log the values.
+
+    Returns
+    -------
+
+    ### train_r2, test_r2
+    '''
+
+    model.eval()
+    with torch.no_grad():
+
+        train_r2 = r2_score(model(x_train), y_train).item()
+        test_r2 = r2_score(model(x_test), y_test).item()
+
+    if do_logging:
+        logging.info("\nR2:")
+        logging.info("train evaluation:")
+        logging.info(train_r2)
+        logging.info("test evaluation:")
+        logging.info(test_r2)
+
+    return [train_r2, test_r2]
+
 def evaluate(model, inputs, outputs, batch_size, loss_func):
 
 
     with torch.no_grad():
+
         epoch_loss = 0
 
         model.eval()
@@ -148,11 +178,6 @@ def evaluate(model, inputs, outputs, batch_size, loss_func):
         for i in range(num_batches):
             x = inputs[i*batch_size:(i+1)*batch_size,:]
             y = outputs[i*batch_size:(i+1)*batch_size,:]
-
-            # print("x:")
-            # print(x)
-            # print("y:")
-            # print(y)
 
             pred = model(x)
 
@@ -226,8 +251,8 @@ def main():
     data_files = os.listdir(folder_path)
     data_files.remove("metadata.txt")
 
-    train_size = 1900
-    test_size = 10
+    train_size = 1800
+    test_size = 200
 
     logging.info(f"train size: {train_size}")
 
@@ -243,10 +268,12 @@ def main():
     x_train = get_train_or_test(folder_path, train_files)
     x_test = get_train_or_test(folder_path, test_files)
 
-    num_of_channels = 200
+    num_of_channels = x_train[0].shape[0]
     take_average_over = 5
-    process_input(x_train, num_of_channels, take_average_over=take_average_over)
-    process_input(x_test, num_of_channels, take_average_over=take_average_over)
+    start_index = 0
+    process_input(x_train, num_of_channels, take_average_over=take_average_over, start_index=start_index)
+    process_input(x_test, num_of_channels, take_average_over=take_average_over, start_index=start_index)
+
 
     # need to use float32 due to problems with mismatch, but should not be a problem
     conv_to_tensor = lambda val: torch.tensor(np.array(val), dtype=torch.float32)
@@ -264,6 +291,9 @@ def main():
     y_train = get_components(metadata, train_files, output_to_fit)
     y_test = get_components(metadata, test_files, output_to_fit)
 
+    y_train = conv_to_tensor(y_train)
+    y_test = conv_to_tensor(y_test)
+
     # normalise to [0,1]
     # y_train = conv_to_tensor(y_train)
     # y_train_col_max = y_train.amax(dim=0)
@@ -271,18 +301,18 @@ def main():
     # y_test = conv_to_tensor(y_test)/y_train_col_max
 
     
-    match output_to_fit:
-        case "intensities":
+    # match output_to_fit:
+    #     case "intensities":
 
-            y_train = conv_to_tensor(y_train)*10
-            y_test = conv_to_tensor(y_test)*10
-            y_train[:,-1] *= 10
-            y_test[:,-1] *= 10
-        case "lifetimes":
-            y_train = conv_to_tensor(y_train)/100
-            y_test = conv_to_tensor(y_test)/100
-            y_train[:,-1] /= 10
-            y_test[:,-1] /= 10
+    #         y_train *= 10
+    #         y_test *= 10
+    #         y_train[:,-1] *= 10
+    #         y_test[:,-1] *= 10
+    #     case "lifetimes":
+    #         y_train /= 100
+    #         y_test /= 100
+    #         y_train[:,-1] /= 10
+    #         y_test[:,-1] /= 10
 
 
 
@@ -298,43 +328,54 @@ def main():
 
     layer_sizes = []
     layer_sizes.append(input_size)
-    # decrease hidden layer size each layer
-    hidden_layer_sizes = [150-i*20 for i in range(7)]
+    # decrease hidden layer size each layer TODO: maybe test with different values here?
+    hidden_layer_sizes = [150-i*15 for i in range(10)]
     layer_sizes.extend(hidden_layer_sizes)
     layer_sizes.append(output_size)
+
+    logging.info(f"layer sizes: {layer_sizes}")
 
     model = MLP(layer_sizes)
     loss_func = torch.nn.MSELoss()
     optim = torch.optim.Adam(model.parameters(), lr=0.001)
     # optim = torch.optim.LBFGS(model.parameters(), max_iter=5, lr=0.01)
 
-    batch_size = 100
+    logging.info("Optimiser:")
+    logging.info(optim)
+
+    batch_size = train_size//10
     logging.info(f"\nBatch size: {batch_size}")
 
     evaluate(model, x_train[:15,], y_train[:15,], batch_size, loss_func)
 
-    epochs = 2000
+    epochs = 4000
     losses = [0]*epochs
     logging.info(f"\nEpochs: {epochs}")
     start = time.time()
-    tolerance = 1e-5
+    tolerance = 1e-7
+    logging.info(f"tolerance: {tolerance}")
     previous_loss = np.inf
+    r2_scores = []
     for epoch in range(epochs):
         print("\033[K",end="")
         print(f"{epoch+1}/{epochs}", end="\r")
-        # train_with_closure(model, x_train, y_train, 5, optim, loss_func)
+        # current_loss = train_with_closure(model, x_train, y_train, batch_size, optim, loss_func)
         current_loss = train(model, x_train, y_train, batch_size, optim, loss_func)
         losses[epoch] = current_loss
+
+        if 0 == epoch%10:
+            r2_scores.append(
+                np.array([float(epoch)]+r2_evaluate(model, x_train, y_train, x_test, y_test))
+            )
 
         # tolerance
         if abs(current_loss-previous_loss)/previous_loss <= tolerance:
             losses = losses[:epoch]
-            logging.info(f"Total epochs:{epoch}")
             break
 
         previous_loss = current_loss
         
-
+    logging.info(f"Total epochs run: {epoch+1}")
         
     stop = time.time()
     logging.info(f"Fitting took {stop-start} seconds")
@@ -342,22 +383,49 @@ def main():
     # y_train *= y_train_col_max
     # y_test *= y_train_col_max
 
-    logging.info("\n train evaluation:")
+    r2_only = True
+    if r2_only:
+        r2_evaluate(model, x_train, y_train, x_test, y_test, do_logging=True)
+    else:
+        logging.info("\n train evaluation:")
+        evaluate(model, x_train[:15,], y_train[:15,], batch_size, loss_func)
+        logging.info("\n test evaluation:")
+        evaluate(model, x_test[:15,], y_test[:15,], batch_size, loss_func)
 
-    evaluate(model, x_train[:15,], y_train[:15,], batch_size, loss_func)
 
-    logging.info("\n test evaluation:")
+    fig, ax = plt.subplots(2,1)
 
-    evaluate(model, x_test[:15,], y_test[:15,], batch_size, loss_func)
+    ax[0].plot(np.arange(len(losses))+1,losses)
+    ax[0].set_xlabel("epoch")
+    ax[0].set_ylabel("epoch loss")
+    ax[0].set_yscale("log")
 
-    plt.plot(np.arange(len(losses))+1,losses)
-    plt.xlabel("epoch")
-    plt.ylabel("epoch loss")
-    plt.yscale("log")
+
+    # first column has epoch, other two have train and test r2
+    r2_scores = np.array(r2_scores)
+    train_r2 = r2_scores[:,1]
+
+    # include only scores greater than zero
+    r2_geq_zero = np.nonzero(train_r2 >= 0)
+
+    train_r2 = train_r2[r2_geq_zero]
+    epoch_r2 = r2_scores[:,0][r2_geq_zero]
+    test_r2 = r2_scores[:,2][r2_geq_zero]
+
+    logging.info("Max test R2:")
+    logging.info(test_r2.max())
+
+    ax[1].plot(epoch_r2, train_r2, label="Train R2")
+    ax[1].plot(epoch_r2, test_r2, label="Test R2")
+    ax[1].set_xlabel("epoch")
+    ax[1].set_ylabel("r2 score")
+    ax[1].legend()
+    ax[1].grid()
     plt.show()
 
 
-
+    #TODO: test optimiser stuff, make adaptive learning rate? test
+    # LBFGS again
 
 
 if __name__ == "__main__":
