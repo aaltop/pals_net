17a124893058b2c854d74202a2a1206df2229098
Topi Aaltonen <topi.aaltonen@helsinki.fi>
2023-04-26 15:21:34 +0300

    Add function for reading new format of simulation data
    In relation to the previous commit. Now both the counts and the
    components are read and returned at the same time, as they're both in
    the same file. Seems like it might be a little faster to read the data
    as well, but that remains to be seen.
    
    Still need to update things elsewhere (train.py, evaluate.py) to
    reflect these changes.

--- read_data.py
+++ read_data.py
@@ -4,16 +4,18 @@ import os
 import numpy as np
 
 def get_data_files(
-        data_folder,
-        train_size,
-        test_size,
-        rng,
+        data_folder:str,
+        train_size:int,
+        test_size:int,
+        rng:np.random.Generator=None,
         permute_data=False
     ):
 
     '''
     Gets the data files (names of data files) in <data_folder>,
     optionally does permutation to shuffle train and test.
+
+    
 
     Returns
     -------
@@ -22,9 +24,16 @@ def get_data_files(
         Names of the train and test files in <data_folder>.
     '''
 
+    if permute_data and rng is None:
+        rng = np.random.default_rng()
+
     folder_path = os.path.join(os.getcwd(), data_folder)
     data_files = os.listdir(folder_path)
-    data_files.remove("metadata.txt")
+
+    for to_remove in ("metadata.txt", "metadata.json"):
+        if to_remove in data_files:
+            data_files.remove(to_remove)
+
 
 
     if permute_data:
@@ -52,7 +61,8 @@ def read_metadata(folder_path, metadata_name=None) -> dict:
 
 def real_meta_to_json(file_path):
     '''
-    Get real metadata as proper dict
+    Get real metadata (metadata that came with the real experiment data)
+    as proper json.
     '''
 
     with open(file_path, "r", encoding="utf-8") as f:
@@ -162,9 +172,6 @@ def read_data(folder_path, file_name, col_names=None) -> pd.DataFrame:
         data = pd.read_table(f,sep=" ", names=col_names, skiprows=1, index_col=False)
 
 
-    # print(file_name)
-    # print(data)
-    # print()
 
     return data
 
@@ -204,6 +211,68 @@ def get_train_or_test(folder_path:str, file_names, col_names=None):
         data[i] = read_data(folder_path, file_name, col_names)["counts"].to_numpy()
 
     return data
+
+def get_simdata(folder_path:str, file_names:list[str]):
+    '''
+    Returns both the simulation counts and the inputs used for the
+    simulation, both of which should be in one file. The inputs
+    (simulation input parameters) should be on the first line of the
+    file as a (json deserialisable) dictionary, and the simulation
+    time and counts make up the rest of the file, in two columns.
+
+    Parameters
+    ----------
+
+    ### folder_path : string
+        The path of the folder the data files are in.
+
+
+    ### file_names : list of strings
+        The names of the files to get the data from.
+
+    Returns
+    -------
+
+    ### counts, components : numpy array
+        The counts of the spectra and the components (lifetimes and
+        intensities) of the spectra.
+    '''
+
+    components = [0]*len(file_names)
+    counts = [0]*len(file_names)
+
+    # in Python 3.11, this (try-finally) could be replaced by contextlib.chdir,
+    # and could of course be written as a context manager here as well
+    # supposedly not thread-safe
+    try:
+        # change to data folder so don't have to create the path
+        # strings for every data file
+        old_path = os.getcwd()
+        os.chdir(folder_path)
+
+        for i,file in enumerate(file_names):
+            with open(file, "r", encoding="utf-8") as f:
+
+                # get the components (lifetimes and intensities)
+                input_prm_str = f.readline()
+                input_prm = json.loads(input_prm_str)
+                comp_list = input_prm["components"]
+                comps = [
+                    elem for sublist in comp_list for elem in sublist
+                ]
+                components[i] = comps
+
+                # get the counts
+                count_col = np.loadtxt(f, dtype="float64", usecols=1)
+                counts[i] = count_col
+
+        return np.array(counts), np.array(components)
+
+    finally:
+        os.chdir(old_path)
+
+
+        
 
 def test_metadata():
     folder = os.path.join(os.getcwd(), "simdata")
@@ -255,6 +324,32 @@ def test_read_real():
 
     # print(read_data(real_folder, "data_0005.pals"))
 
+def test_get_simdata():
+
+    import time
+
+    start = time.time()
+
+    data_folder = "simdata_test"
+    train_files, test_files = get_data_files(
+        data_folder,
+        train_size=500,
+        test_size=500,
+        )
+    
+    folder_path = os.path.join(
+        os.getcwd(),
+        data_folder
+    )
+
+    train_counts, train_components = get_simdata(folder_path, train_files)
+    test_counts, test_components = get_simdata(folder_path, test_files)
+
+    print(f"Took {time.time()-start} seconds.")
+
+    print(train_counts.shape)
+    print(train_components.shape)
+
 def main():
 
     folder_path = os.path.join(os.getcwd(), "simdata")
@@ -270,4 +365,4 @@ def main():
 
 if __name__ == "__main__":
 
-    test_read_real()
+    test_get_simdata()
